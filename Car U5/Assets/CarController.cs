f66152727dbad08c9fc6e3f96f43aa9671d117ed
Dennis McWilliams <d12768@gmail.com>
2015-09-20 15:29:52 -0400

    First

---
+++ Car U5/Assets/CarController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+    public Transform centerOfMass;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    public WheelCollider wheelBL;
+    public WheelCollider wheelBR;
+
+    public float maxTorque = 40;
+    public float maxSteerAngle = 20;
+    Rigidbody rBody;
+    void Awake()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start () {
+        rBody.centerOfMass = centerOfMass.localPosition;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        wheelBR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	    wheelBL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+
+        wheelFL.steerAngle = maxSteerAngle * Input.GetAxis("Horizontal");
+        wheelFR.steerAngle = maxSteerAngle * Input.GetAxis("Horizontal");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CarU5
 First attempt at a Unity 5 multiplayer car game
+
+Test - edit from pc
