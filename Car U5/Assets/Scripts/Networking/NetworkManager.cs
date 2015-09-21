037f72f3a87bed2d3a5d5651ed73d110a0628f6a
Dennis McWilliams <d12768@gmail.com>
2015-09-20 20:50:51 -0400

    no message

---
+++ Car U5/Assets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	const string VERSION = "v0.0.1";
+	public string roomName = "CarU5";
+	
+	public string playerPrefabName = "Car";
+	public Transform SpawnPoint;
+	
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings(VERSION);
+		Debug.Log("NetWork Start");
+	}
+	
+	void OnJoinedLobby(){
+		RoomOptions roomOptions = new RoomOptions(){ isVisible =false, maxPlayers = 4};
+		PhotonNetwork.JoinOrCreateRoom (roomName, roomOptions, TypedLobby.Default);
+	}
+	
+	void OnJoinedRoom(){
+		Debug.Log("instantiating" + playerPrefabName);
+		PhotonNetwork.Instantiate(	playerPrefabName,
+		 							SpawnPoint.position,
+									SpawnPoint.rotation, 0);
+		Debug.Log("instantiated" + playerPrefabName);
+	}
+}
+
---
+++ Car U5/Assets/Scripts/Networking/NetworkPlayer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+	public GameObject myCamera;
+	bool isAlive = true;
+	Vector3 position;
+	Quaternion rotation;
+	public float lerpSmoothing = 5f;
+	
+	void Start(){
+		bool isLocalPlayer = photonView.isMine;
+		Debug.Log("local player:" + isLocalPlayer.ToString());
+
+		myCamera.SetActive(isLocalPlayer);
+		GetComponent<SimpleCarController>().enabled = isLocalPlayer;
+		GetComponent<Rigidbody>().useGravity = true; 
+
+        if (!isLocalPlayer)
+        {
+            position = transform.position;
+            rotation = transform.rotation;
+            StartCoroutine(Alive());
+            Debug.Log("started corouting");
+        }
+
+	}
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        Debug.Log("OnPhotonSerializeView");
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            Vector3 newPosition = (Vector3)stream.ReceiveNext();
+            Quaternion newRotation = (Quaternion)stream.ReceiveNext();
+            if (newPosition != null) position = newPosition;
+            if (rotation != null) rotation = newRotation;
+            
+        }
+    }
+
+
+    IEnumerator Alive()
+    {
+        while (isAlive)
+        {
+            transform.position = Vector3.Lerp (transform.position, position, Time.deltaTime * lerpSmoothing);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * lerpSmoothing);
+            yield return null;
+        }
+    }
+
+}
